dd1dfd3f9a1c0bcfa57d1db6f9551480de4a5a5c
NetaCohen4 <118432781+NetaCohen4@users.noreply.github.com>
2024-01-30 20:55:51 +0200

    Update OS_1_1.c

--- OS_1_1.c
+++ OS_1_1.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
  int main(){
 
-  int *ptr = 0xdeadbeef;
-  *ptr=5;
-
-//   printf("value at the invalid memory location: ");
-//   printf("%d\n", *ptr);
- //  return 0;
+  int *ptr = NULL;
+   
+  printf("value at the invalid memory location: ");
+  printf("%d\n", *ptr);
+  return 0;
  }
  
  
